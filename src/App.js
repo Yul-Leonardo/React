cfd2e1239942fd783d3a70c964e336ae28b87217
Yul-Leonardo <46727018+Yul-Leonardo@users.noreply.github.com>
2022-03-08 19:55:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from './components/Nav';
+import Greeter from './components/Greeter';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Greeter maxLength={20} />
       <Nav />
 
     </div>
