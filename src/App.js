0fb3f08c97940d3f0f4fac824fb91574821b2c02
Yul-Leonardo <46727018+Yul-Leonardo@users.noreply.github.com>
2022-03-08 20:21:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Nav from './components/Nav';
 import Greeter from './components/Greeter';
+import ChoiceBar from './components/ChoiceBar';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Greeter maxLength={20} />
-      <Nav />
+      {/* <Greeter maxLength={20} />
+      <Nav /> */}
+      <ChoiceBar title="JavaScript" percent={37} />
 
     </div>
   );
